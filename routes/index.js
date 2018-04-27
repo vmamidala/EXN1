1ddd4aef20f63852b426990f363a047e36f8d541
vijay <vijaymamidala93@gmail.com>
2018-04-26 20:36:20 -0700

    FirstCommit-2

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
 
 
 //**************************************************************************
